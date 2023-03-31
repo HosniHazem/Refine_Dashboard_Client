097c1c9f58d3cee2e035f9110d585ebdd323862e
Hazem Hosni <hazem.hosni@aiesec.net>
2023-03-31 12:04:32 +0100

    initial commit with superplate

---
+++ src/components/header/index.tsx
@@ -0,0 +1,91 @@
+import { DarkModeOutlined, LightModeOutlined, Menu } from "@mui/icons-material";
+import {
+  AppBar,
+  Avatar,
+  IconButton,
+  Stack,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { useGetIdentity } from "@refinedev/core";
+import { RefineThemedLayoutHeaderProps } from "@refinedev/mui";
+import React, { useContext } from "react";
+
+import { ColorModeContext } from "../../contexts/color-mode";
+
+type IUser = {
+  id: number;
+  name: string;
+  avatar: string;
+};
+
+export const Header: React.FC<RefineThemedLayoutHeaderProps> = ({
+  isSiderOpen,
+  onToggleSiderClick,
+  toggleSiderIcon: toggleSiderIconFromProps,
+}) => {
+  const { mode, setMode } = useContext(ColorModeContext);
+
+  const { data: user } = useGetIdentity<IUser>();
+
+  const hasSidebarToggle = Boolean(onToggleSiderClick);
+
+  return (
+    <AppBar position="sticky">
+      <Toolbar>
+        <Stack
+          direction="row"
+          width="100%"
+          justifyContent="flex-end"
+          alignItems="center"
+        >
+          {hasSidebarToggle && (
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={() => onToggleSiderClick?.()}
+              edge="start"
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                ...(isSiderOpen && { display: "none" }),
+              }}
+            >
+              {toggleSiderIconFromProps?.(Boolean(isSiderOpen)) ?? <Menu />}
+            </IconButton>
+          )}
+
+          <Stack
+            direction="row"
+            width="100%"
+            justifyContent="flex-end"
+            alignItems="center"
+          >
+            <IconButton
+              color="inherit"
+              onClick={() => {
+                setMode();
+              }}
+            >
+              {mode === "dark" ? <LightModeOutlined /> : <DarkModeOutlined />}
+            </IconButton>
+
+            {(user?.avatar || user?.name) && (
+              <Stack
+                direction="row"
+                gap="16px"
+                alignItems="center"
+                justifyContent="center"
+              >
+                {user?.name && (
+                  <Typography variant="subtitle2">{user?.name}</Typography>
+                )}
+                <Avatar src={user?.avatar} alt={user?.name} />
+              </Stack>
+            )}
+          </Stack>
+        </Stack>
+      </Toolbar>
+    </AppBar>
+  );
+};
---
+++ src/pages/login.tsx
@@ -0,0 +1,78 @@
+import { useLogin } from "@refinedev/core";
+import { useEffect, useRef } from "react";
+
+import { Box, Container, Typography } from "@mui/material";
+import { ThemedTitle } from "@refinedev/mui";
+
+import { CredentialResponse } from "../interfaces/google";
+
+// Todo: Update your Google Client ID here
+const GOOGLE_CLIENT_ID =
+  "1041339102270-jlljcjl19jo1hkgf695em3ibr7q2m734.apps.googleusercontent.com";
+
+export const Login: React.FC = () => {
+  const { mutate: login } = useLogin<CredentialResponse>();
+
+  const GoogleButton = (): JSX.Element => {
+    const divRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+      if (typeof window === "undefined" || !window.google || !divRef.current) {
+        return;
+      }
+
+      try {
+        window.google.accounts.id.initialize({
+          ux_mode: "popup",
+          client_id: GOOGLE_CLIENT_ID,
+          callback: async (res: CredentialResponse) => {
+            if (res.credential) {
+              login(res);
+            }
+          },
+        });
+        window.google.accounts.id.renderButton(divRef.current, {
+          theme: "filled_blue",
+          size: "medium",
+          type: "standard",
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }, []);
+
+    return <div ref={divRef} />;
+  };
+
+  return (
+    <Container
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Box
+        display="flex"
+        gap="25px"
+        justifyContent="center"
+        flexDirection="column"
+      >
+        <ThemedTitle
+          collapsed={false}
+          wrapperStyles={{
+            fontSize: "22px",
+            justifyContent: "center",
+          }}
+        />
+
+        <GoogleButton />
+
+        <Typography align="center" color={"text.secondary"} fontSize="12px">
+          Powered by Auth0
+        </Typography>
+      </Box>
+    </Container>
+  );
+};
